4f9ee4dab5579da603ba38dc62a9c0100b8d9859
Nasser Mokadem <nmokadem@hotmail.com>
2021-11-09 14:52:52 +0000

    Modify delete button to urls_edit and modify urls_show to allow editing of urls

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const HOST = 'localhost';
 
 app.set("view engine", "ejs");
 
@@ -21,11 +22,14 @@ function generateRandomString() {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
+
+// A router to list all the urls in the database
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
+// Router to add a new record
 app.post("/urls", (req, res) => {
   //console.log(req.body);  // Log the POST request body to the console
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
@@ -36,25 +40,39 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
+// Router to display a form to add a new record
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Router to edit a record from the database
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  //res.render("urls_show", templateVars);
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  const templateVars = 
+    { 
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL],
+      host : HOST,
+      port : PORT 
+    };
+  res.render("urls_show", templateVars);
+  // let longURL = urlDatabase[req.params.shortURL];
+  // res.redirect(longURL);
 });
 
+// Router to delete a record from the database
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+// Router to update a record from the database
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  //console.log('...............',shortURL,longURL);
+  //console.log(req.body.longURL);
+  urlDatabase[shortURL] = longURL;
   res.redirect("/urls");
 });
 
@@ -70,6 +88,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //  app.get("/fetch", (req, res) => {
 //   res.send(`a = ${a}`);
 //  });
+
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: 'Hello World!' };
+//   res.render("hello_world", templateVars);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
