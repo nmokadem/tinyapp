23064f6f52320a2bbc5adcb088ccab1381e81034
Nasser Mokadem <nmokadem@hotmail.com>
2021-12-07 22:28:46 +0000

    fix the error Receiving an error, cannot access other accounts' short URLs, the login issue with user@exampl.com, change routers from registration to register

--- express_server.js
+++ express_server.js
@@ -2,19 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 //const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
-const morgan = require('morgan');
-const bcrypt = require('bcryptjs');
-const methodOverride = require('method-override');
-const fs = require('fs');
+const morgan = require("morgan");
+const bcrypt = require("bcryptjs");
+const methodOverride = require("method-override");
+const fs = require("fs");
 
-const { generateRandomString, urlsForUser } = require('./helpers');
-let { PORT, urlDatabase, users, alerts } = require('./setup');
+const { generateRandomString, urlsForUser } = require("./helpers");
+let { PORT, urlDatabase, users, alerts } = require("./setup");
 const app = express();
 
 let userId = "";
 let alert = "";
 //let statistics = {};
-
 
 //============================================================
 //functions to read and write from file for users and URLs
@@ -24,74 +23,72 @@ let alert = "";
 //
 // IIFE to read users rom files
 (() => {
-  fs.readFile('users.json', 'utf-8', (err, data) => {
+  fs.readFile("users.json", "utf-8", (err, data) => {
     if (err) {
       throw err;
     }
 
     // Parse JSON object
-    if (data.length) {               // data is not empty
+    if (data.length) {
+      // data is not empty
       users = JSON.parse(data.toString());
     } else {
-      users =
-        {
-          "userRandomID": {
-            "id": "userRandomID",
-            "email": "user@example.com",
-            "password": "purple-monkey-dinosaur"
-          },
-          "user2RandomID": {
-            "id": "user2RandomID",
-            "email": "user2@example.com",
-            "password": "dishwasher-funk"
-          }
-        };
-    }
-  });
-})();
-
-
-//IIFE to read urls from files
-(() => {
-  fs.readFile('urls.json', 'utf-8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-
-    // Parse JSON object
-    if (data.length) {               // data is not empty
-      urlDatabase = JSON.parse(data.toString());
-    } else {
-      urlDatabase = {
-        b6UTxQ: {
-          longURL: "https://www.tsn.ca",
-          userID: "aJ48lW"
+      users = {
+        userRandomID: {
+          id: "userRandomID",
+          email: "user@example.com",
+          password: "purple-monkey-dinosaur",
         },
-        i3BoGr: {
-          longURL: "https://www.google.ca",
-          userID: "aJ48lW"
-        }
+        user2RandomID: {
+          id: "user2RandomID",
+          email: "user2@example.com",
+          password: "dishwasher-funk",
+        },
       };
     }
   });
 })();
 
+//IIFE to read urls from files
+(() => {
+  fs.readFile("urls.json", "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    // Parse JSON object
+    if (data.length) {
+      // data is not empty
+      urlDatabase = JSON.parse(data.toString());
+    } else {
+      urlDatabase = {
+        b6UTxQ: {
+          longURL: "https://www.tsn.ca",
+          userID: "aJ48lW",
+        },
+        i3BoGr: {
+          longURL: "https://www.google.ca",
+          userID: "aJ48lW",
+        },
+      };
+    }
+  });
+})();
 
 // Save Users to file
 const saveUsers = () => {
   const data = JSON.stringify(users);
-  fs.writeFile('users.json', data, (err) => {
+  fs.writeFile("users.json", data, (err) => {
     if (err) {
       throw err;
     }
   });
 };
 
-
 // Save URLs to file
 const saveURLs = () => {
   const data = JSON.stringify(urlDatabase);
-  fs.writeFile('urls.json', data, (err) => {
+  fs.writeFile("urls.json", data, (err) => {
     if (err) {
       throw err;
     }
@@ -102,34 +99,36 @@ const saveURLs = () => {
 
 app.set("view engine", "ejs");
 
-
 //*******************************************************************
 // middlewares
 //*******************************************************************
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(cookieParser());
-app.use(cookieSession(
-  {name : 'session',
-    keys : ['key1', 'key2'],
-  //maxAge : 5 * 60 *1000    //24 * 60 * 60 * 1000 // 24 hours
-  }
-));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+    //maxAge : 5 * 60 *1000    //24 * 60 * 60 * 1000 // 24 hours
+  })
+);
 
 //app.use(morgan('combined'));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Middleware to be used for HTTP DELETE and PUT
 //app.use(methodOverride('X-HTTP-Method-Override'));
 //app.use(methodOverride('_method'));  // this works fine with delete but not put
-app.use(methodOverride(function(req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    let method = req.body._method;
-    delete req.body._method;
-    return method;
-  }
-}));
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 // Just to view that this is a middleware
 app.use((req, res, next) => {
@@ -141,7 +140,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
 //*******************************************************************
 // Functions to be separated in a different module
 //*******************************************************************
@@ -149,9 +147,9 @@ app.use((req, res, next) => {
 //clear cookies set by cookie-session
 //Two cookies need to be clear 'session' and session.sig (cookie-session)
 const clearCookies = (res) => {
-  res.clearCookie('session');
-  res.clearCookie('session.sig');
-  userId = '';                          //reset the global userId
+  res.clearCookie("session");
+  res.clearCookie("session.sig");
+  userId = ""; //reset the global userId
 };
 
 //create cookies set by cookie-session
@@ -174,18 +172,16 @@ const getUserFromCookie = (req) => {
         return users[key];
       }
     }
-    userId = '';
+    userId = "";
   }
   return {};
 };
 
-
-const routeURLHelper = function(req, res) {
-
-  let email = '';
+const routeURLHelper = function (req, res) {
+  let email = "";
   let user = getUserFromCookie(req);
 
-  email = user['email'];
+  email = user["email"];
   if (!email) {
     alert = alerts["alert8"];
   }
@@ -193,12 +189,11 @@ const routeURLHelper = function(req, res) {
   const templateVars = {
     userId,
     email,
-    alert
+    alert,
   };
 
   return templateVars;
 };
-
 
 // Called from the routers PUT/POST of users
 const postOrPutUser = (method, req, res) => {
@@ -213,8 +208,10 @@ const postOrPutUser = (method, req, res) => {
   if (id && email && password) {
     let keys = Object.keys(users);
     for (const key of keys) {
-      if ((method === 'post' && users[key].email === email) ||
-          (method === 'put' && users[key].email === email && users[key].id !== id)) {
+      if (
+        (method === "post" && users[key].email === email) ||
+        (method === "put" && users[key].email === email && users[key].id !== id)
+      ) {
         alert = alerts["alert2"];
         break;
       }
@@ -224,63 +221,71 @@ const postOrPutUser = (method, req, res) => {
       thisObj = {
         id,
         email,
-        password : hashedPassword
+        password: hashedPassword,
       };
 
       //POST or PUT profile (no change of id so no need to delete first in case of an update)
       users[id] = thisObj;
-      saveUsers();                                // Save to file
-      sendCookie(req, res, 'userId', email);      //set new cookie
+      saveUsers(); // Save to file
+      sendCookie(req, res, "userId", email); //set new cookie
     }
   } else {
     alert = alerts["alert5"];
   }
 
   if (alert) {
-    res.redirect("/registration");         //An error occured Restart the registration
+    res.redirect("/register"); //An error occured Restart the register
   } else {
     res.redirect("/urls");
   }
 };
 
-
 // Called from the routers PUT/POST of URLs
-const postOrPutURL = (method,req, res) => {
+const postOrPutURL = (method, req, res) => {
   alert = "";
   let user = getUserFromCookie(req);
 
   let shorturl = req.body.shortURL;
   let longurl = req.body.longURL;
+  if (longurl) {
+    //URL not empty or else do nothing
 
-  if (longurl) {   //URL not empty or else do nothing
-
-    if (!shorturl) {  //Add a new URL
+    if (!shorturl) {
+      //Add a new URL
       shorturl = generateRandomString(6);
-      res.redirect("/urls/" + shorturl);
+      //res.redirect("/urls/" + shorturl);
     }
 
-    if (longurl.length > 5) {  //? what if it is shorter
-      if (longurl.substring(0,4) !== 'http') {
-        if (longurl.substr(0,3) !== 'www') {
+    if (longurl.length > 5) {
+      //? what if it is shorter
+      if (longurl.substring(0, 4) !== "http") {
+        if (longurl.substr(0, 3) !== "www") {
           longurl = "www." + longurl;
         }
-        longurl = 'http://' + longurl;
+        longurl = "http://" + longurl;
       }
     }
 
     //Check if owner of the URL is the same as the current user
     // Permisson denied in case they are not the same. Cannot edit record
-    const urlUserID = urlDatabase[shorturl].userID;                // Owner of the URL
-    if (method === "put" && user['id'] !== urlUserID) {
-      alert = alerts["alertB"];
-      res.redirect("/urls/" + shorturl);
-      return;
+
+    if (method === "put") {
+      const urlUserID = urlDatabase[shorturl].userID; // Owner of the URL
+      if (user["id"] !== urlUserID) {
+        alert = alerts["alertB"];
+        res.redirect("/urls/" + shorturl);
+        return;
+      }
     }
 
     for (const url in urlDatabase) {
-      if ((method === "put" && urlDatabase[url].longURL === longurl && url !== shorturl) ||
-         (method === "post" && urlDatabase[url].longURL === longurl)) {
-        alert = alerts["alert3"];    //URL entered already exists
+      if (
+        (method === "put" &&
+          urlDatabase[url].longURL === longurl &&
+          url !== shorturl) ||
+        (method === "post" && urlDatabase[url].longURL === longurl)
+      ) {
+        alert = alerts["alert3"]; //URL entered already exists
         break;
       }
     }
@@ -288,7 +293,7 @@ const postOrPutURL = (method,req, res) => {
     if (alert === "") {
       let thisObj = {};
       thisObj.longURL = longurl;
-      thisObj.userID = user['id'];   //userId;
+      thisObj.userID = user["id"]; //userId;
       urlDatabase[shorturl] = thisObj;
       saveURLs();
     }
@@ -321,21 +326,20 @@ app.get("/", (req, res) => {
 
 // Router used to login
 app.get("/login", (req, res) => {
-  userId = '';
-  const email = '';   // Global
-  const password = '';
+  userId = "";
+  const email = ""; // Global
+  const password = "";
   // alert = '';
 
   const templateVars = {
     userId,
     email,
     password,
-    alert
+    alert,
   };
-  alert = '';
+  alert = "";
   res.render("login", templateVars);
 });
-
 
 // Router to validate email and password
 app.post("/login", (req, res) => {
@@ -348,14 +352,18 @@ app.post("/login", (req, res) => {
   if (email && password) {
     let keys = Object.keys(users);
     for (const key of keys) {
-      if (users[key].email === email && bcrypt.compareSync(password, users[key].password)) {
-        sendCookie(req, res, 'userId', email);
+      if (
+        users[key].email === email &&
+        bcrypt.compareSync(password, users[key].password)
+      ) {
+        sendCookie(req, res, "userId", email);
         user = users[key];
         break;
       }
     }
 
-    if (Object.keys(user).length === 0) {      // check if user is an empty object
+    if (Object.keys(user).length === 0) {
+      // check if user is an empty object
       alert = alerts["alert1"];
     }
   }
@@ -368,7 +376,6 @@ app.post("/login", (req, res) => {
   }
 });
 
-
 //Router used to log out
 app.post("/logout", (req, res) => {
   clearCookies(res);
@@ -377,11 +384,11 @@ app.post("/logout", (req, res) => {
 
 //=========================================================================
 
-//Router used to display either registration.ejs to edit a profile or
-//                              registration_new.ejs to register a new profile
-app.get("/registration", (req, res) => {
-  let email = '';
-  let password = '';
+//Router used to display either register.ejs to edit a profile or
+//                              register_new.ejs to register a new profile
+app.get("/register", (req, res) => {
+  let email = "";
+  let password = "";
   let user = getUserFromCookie(req);
 
   if (user.id) {
@@ -396,69 +403,65 @@ app.get("/registration", (req, res) => {
     userId,
     email,
     password,
-    alert
+    alert,
   };
 
   if (email) {
-    res.render("registration", templateVars);     //Edit existing profile
+    res.render("register", templateVars); //Edit existing profile
   } else {
-    res.render("registration_new", templateVars); //Create new profile
+    res.render("register_new", templateVars); //Create new profile
   }
 });
 
-
 // router used to post/add a new profile
-app.post("/registration", (req, res) => {
+app.post("/register", (req, res) => {
   postOrPutUser("post", req, res);
 });
 
 // router used to put/edit profile
-app.put("/registration", (req, res) => {
+app.put("/register", (req, res) => {
   postOrPutUser("put", req, res);
 });
-
 
 // Routers for URLs
 //=====================================================
 //router used to display list of urls
 app.get("/urls", (req, res) => {
-  let email = '';
+  let email = "";
   let urls = {};
 
   let user = getUserFromCookie(req);
 
-  if (user['email']) {
-    email = user['email'];
+  if (user["email"]) {
+    email = user["email"];
     urls = urlsForUser(user.id, urlDatabase);
   }
 
-
   if (!email) {
-    alert = alerts["alert7"];      //Login first
+    alert = alerts["alert7"]; //Login first
   } else if (Object.keys(urls).length === 0) {
-    alert = alerts["alert6"];         //No urls found
+    alert = alerts["alert6"]; //No urls found
   }
-  
+
   const templateVars = {
     userId,
     email,
     alert,
-    urls
+    urls,
   };
   alert = "";
 
   res.render("urls_index", templateVars);
 });
 
-
 //router used to edit urls
 app.put("/urls", (req, res) => {
-  postOrPutURL('put',req, res);
+  postOrPutURL("put", req, res);
 });
 
 //router used to add urls
 app.post("/urls", (req, res) => {
-  postOrPutURL('post',req, res);
+  postOrPutURL("post", req, res);
 });
 
 // router used to display new form for url
@@ -466,14 +469,14 @@ app.get("/urls/new", (req, res) => {
   let obj = {};
   obj = routeURLHelper(req, res);
 
-  if (!obj.email) {   // user not logged in
+  if (!obj.email) {
+    // user not logged in
     res.redirect("/login");
     return;
   }
   alert = "";
   res.render("urls_new", obj);
 });
-
 
 // Router used to edit a url record
 app.get("/urls/:shortURL", (req, res) => {
@@ -488,9 +491,9 @@ app.get("/urls/:shortURL", (req, res) => {
 
   if (req.params.shortURL && urlDatabase[req.params.shortURL].longURL) {
     obj.shortURL = req.params.shortURL;
-    obj.longURL  = urlDatabase[req.params.shortURL].longURL;
+    obj.longURL = urlDatabase[req.params.shortURL].longURL;
   } else {
-    alert =  alerts['alertA'];     //record not found
+    alert = alerts["alertA"]; //record not found
     res.redirect("/urls");
     return;
   }
@@ -504,7 +507,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.delete("/urls/:shortURL", (req, res) => {
   let user = getUserFromCookie(req);
 
-  if (!user['id']) {
+  if (!user["id"]) {
     alert = alerts["alert8"];
     res.redirect("/login");
     return;
@@ -517,7 +520,6 @@ app.delete("/urls/:shortURL", (req, res) => {
 
   res.redirect("/urls");
 });
-
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`TinyURLs app ==> listening on port ${PORT}!`);
